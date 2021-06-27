61d01206ecaf9e3aace4acdc70bef9f841cf27ac
Ezeddean Yousif <ezeddean.yousif@hotmail.com>
2021-06-27 12:43:18 +0200

    mutli-device-log

--- ssh_multi_devices_log.py
+++ ssh_multi_devices_log.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 from netmiko import ConnectHandler
-from getpass import getpass
 
-password = getpass()
 
 FWTSR01 = {
     'device_type': 'ericsson_ipos',
@@ -20,18 +18,18 @@ FWTSR02 = {
     "session_log": "FWTSR02_output.txt",
 }
 
+term_len = "term len 0"
+command = "show ip int brief"
 
 for device in (FWTSR01, FWTSR02):
     net_connect = ConnectHandler(**device)
-    output = net_connect.send_command(term_len)
+#    output = net_connect.send_command(term_len)
     output = net_connect.send_command(command)
     print(net_connect.find_prompt())
     net_connect.disconnect()
 
 
 # Show command that we execute
-#term_len = "term len 0"
-#command = "show ip int brief"
 #for device2 in (FWTSR01, FWTSR02):
 #    with ConnectHandler(**device2) as net_connect:
 #        output = net_connect.send_command(term_len)
