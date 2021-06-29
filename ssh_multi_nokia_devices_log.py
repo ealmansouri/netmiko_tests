fde3486a2e8e41e8c1237c580939572deae76971
Ezeddean Yousif <ezeddean.yousif@hotmail.com>
2021-06-29 12:09:27 +0200

    Nokia script working with loop

--- ssh_multi_nokia_devices_log.py
+++ ssh_multi_nokia_devices_log.py
@@ -3,7 +3,7 @@ from netmiko import ConnectHandler
 
 
 BEN_SR7_01 = {
-    'device_type': 'nokia_sros',
+    "device_type": 'nokia_sros',
     'host':   '10.202.129.1',
     'username': 'e.mansouri',
     'password': 'R0YgB1V!00',
@@ -82,6 +82,7 @@ TOB_SRA8_01 = {
     "session_log": "TOB_SRA8_01.txt",
 }
 
+val = input
 
 listBlue = ["30040", "30050", "30110", "30120", "30130", "30200", "30300", "30400"]
 listRed = ["30040", "30050", "30060", "30200", "30300", "30400"]
@@ -91,44 +92,46 @@ listDer1 = ["30040", "30050", "30060", "30110", "30120", "30130", "30200", "3030
 listDer2 = ["30040", "30110", "30120", "30130", "30200", "30300"]
 
 for device in (BEN_SR7_02, FWT_SR7_01, FWT_SR7_02):
+    net_connect = ConnectHandler(**device)
+    termLenCmd = net_connect.send_command("environment no more")
+    shRouteTable = net_connect.send_command("show router route-table protocol local")
     for vprn in listBlue:
-        net_connect = ConnectHandler(**device)
-        termLenCmd = net_connect.send_command("environment no more")
-        shRouteTable = net_connect.send_command("show router " + vprn + " route-table protocol local")
-        #print(net_connect.find_prompt())
-        #net_connect.disconnect()
+        shRouteTable2 = net_connect.send_command("show router " + vprn + " route-table protocol local")
+    print(net_connect.find_prompt())
+    net_connect.disconnect()
 
-#for device in (ALM_SRA8_01, ALB_SRA8_01, TOB_SRA8_01):
-#    for vprn in listRed:
-#        net_connect = ConnectHandler(**device)
-#        termLenCmd = net_connect.send_command("environment no more")
-#        shRouteTable = net_connect.send_command("show router " + vprn + " route-table protocol local")
-#        print(net_connect.find_prompt())
-#        net_connect.disconnect()
-#    
-#for device in (BEN_SR7_01):
-#    for vprn in listGreen:
-#        net_connect = ConnectHandler(**device)
-#        termLenCmd = net_connect.send_command("environment no more")
-#        shRouteTable = net_connect.send_command("show router " + vprn + " route-table protocol local")
-#        print(net_connect.find_prompt())
-#        net_connect.disconnect()
-#
-#for device in (DER_SRA8_01):
-#    for vprn in listDer1:
-#        net_connect = ConnectHandler(**device)
-#        termLenCmd = net_connect.send_command("environment no more")
-#        shRouteTable = net_connect.send_command("show router " + vprn + " route-table protocol local")
-#        print(net_connect.find_prompt())
-#        net_connect.disconnect()
-#
-#for device in (DER_SRA8_02):
-#    for vprn in listDer2:
-#        net_connect = ConnectHandler(**device)
-#        termLenCmd = net_connect.send_command("environment no more")
-#        shRouteTable = net_connect.send_command("show router " + vprn + " route-table protocol local")
-#        print(net_connect.find_prompt())
-#        net_connect.disconnect()
-#
+for device in (ALM_SRA8_01, ALB_SRA8_01, TOB_SRA8_01):
+    net_connect = ConnectHandler(**device)
+    termLenCmd = net_connect.send_command("environment no more")
+    shRouteTable = net_connect.send_command("show router route-table protocol local")
+    for vprn in listRed:
+        shRouteTable = net_connect.send_command("show router " + vprn + " route-table protocol local")
+    print(net_connect.find_prompt())
+    net_connect.disconnect()
+    
+net_connect = ConnectHandler(**BEN_SR7_01)
+termLenCmd = net_connect.send_command("environment no more")
+shRouteTable = net_connect.send_command("show router route-table protocol local")
+for vprn in listGreen:
+    shRouteTable = net_connect.send_command("show router " + vprn + " route-table protocol local")
+print(net_connect.find_prompt())
+net_connect.disconnect()
+
+net_connect = ConnectHandler(**DER_SRA8_01)
+termLenCmd = net_connect.send_command("environment no more")
+shRouteTable = net_connect.send_command("show router route-table protocol local")
+for vprn in listDer1:
+    shRouteTable = net_connect.send_command("show router " + vprn + " route-table protocol local")
+print(net_connect.find_prompt())
+net_connect.disconnect()
+
+net_connect = ConnectHandler(**DER_SRA8_02)
+termLenCmd = net_connect.send_command("environment no more")
+shRouteTable = net_connect.send_command("show router route-table protocol local")
+for vprn in listDer2:
+    shRouteTable = net_connect.send_command("show router " + vprn + " route-table protocol local")
+print(net_connect.find_prompt())
+net_connect.disconnect()
+
 
 
