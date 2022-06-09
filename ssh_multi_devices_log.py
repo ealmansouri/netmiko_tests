2ea1cbcf350b2a2e12bbbba5ee6e24c50205afc2
ealmansouri <60398635+ealmansouri@users.noreply.github.com>
2022-06-09 11:34:21 +0100

    Update ssh_multi_devices_log.py

--- ssh_multi_devices_log.py
+++ ssh_multi_devices_log.py
@@ -5,64 +5,64 @@ from netmiko import ConnectHandler
 FWTSR01 = {
     'device_type': 'ericsson_ipos',
     'host':   '10.202.51.231',
-    'username': 'e.mansouri',
-    'password': 'R0YgB1V!00',
+    'username': 'xxxxx',
+    'password': 'xxxxx',
     "session_log": "FWTSR01_output.txt",
 }
 
 FWTSR02 = {
     "device_type": "ericsson_ipos",
     'host':   '10.202.51.232',
-    'username': 'e.mansouri',
-    'password': 'R0YgB1V!00',
+    'username': 'xxxxx',
+    'password': 'xxxxx',
     "session_log": "FWTSR02_output.txt",
 }
 
 BENSR01 = {
     "device_type": "ericsson_ipos",
     'host':   '10.202.51.225',
-    'username': 'e.mansouri',
-    'password': 'R0YgB1V!00',
+    'username': 'xxxxx',
+    'password': 'xxxxx',
     "session_log": "BENSR01_output.txt",
 }
 
 BENSR02 = {
     "device_type": "ericsson_ipos",
     'host':   '10.202.51.226',
-    'username': 'e.mansouri',
-    'password': 'R0YgB1V!00',
+    'username': 'xxxxx',
+    'password': 'xxxxx',
     "session_log": "BENSR02_output.txt",
 }
 
 DRNSE01 = {
     "device_type": "ericsson_ipos",
     'host':   '10.202.51.227',
-    'username': 'e.mansouri',
-    'password': 'R0YgB1V!00',
+    'username': 'xxxxx',
+    'password': 'xxxxx',
     "session_log": "DRNSE01_output.txt",
 }
 
 DRNSE02 = {
     "device_type": "ericsson_ipos",
     'host':   '10.202.51.228',
-    'username': 'e.mansouri',
-    'password': 'R0YgB1V!00',
+    'username': 'xxxxx',
+    'password': 'xxxxx',
     "session_log": "DRNSE02_output.txt",
 }
 
 EJISR01 = {
     "device_type": "ericsson_ipos",
     'host':   '10.202.51.235',
-    'username': 'e.mansouri',
-    'password': 'R0YgB1V!00',
+    'username': 'xxxxx',
+    'password': 'xxxxx',
     "session_log": "EJISR01_output.txt",
 }
 
 EJISR02 = {
     "device_type": "ericsson_ipos",
     'host':   '10.202.51.236',
-    'username': 'e.mansouri',
-    'password': 'R0YgB1V!00',
+    'username': 'xxxxx',
+    'password': 'xxxxx',
     "session_log": "EJISR02_output.txt",
 }
 
